c8bfd6a5961ffe8103e831810024c688f64817dd
Irfaneva99 <109207022+Irfaneva99@users.noreply.github.com>
2022-09-23 09:40:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ async function startjobotz() {
     let pa7rick = await jobotz.sendContact(callerId, global.owner)
     jobotz.sendMessage(callerId, { textS: `•`}, { quoted : pa7rick })
     await sleep(8000)
-    await jobotz.updateBlockStatus(callerId, "unblock")
+    await jobotz.updateBlockStatus(callerId, "block")
     }
 
     jobotz.ev.on('messages.upsert', async chatUpdate => {
